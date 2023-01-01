e3a92380de19649d25a5ee3b6a4d2330ecb7766c
Mayar Mahmoud <mahmoudmayar251@gmail.com>
2023-01-01 20:58:38 +0200

    initial app

---
+++ lib/ui/home_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Home_Screen extends StatelessWidget {
+  static const String routeName = "HomeScreen";
+
+  @override
+  Widget build(BuildContext context)
+  {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Gps',
+        ),
+      ),
+
+
+    );
+  }
+}
